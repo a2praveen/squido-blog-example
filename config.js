476ab6e2bf69172192185d25f5fe900c9a93bbce
a2praveen <a2praveen@gmail.com>
2022-05-06 12:02:19 +0530

    rr

--- config.js
+++ config.js
@@ -1,10 +1,49 @@
 const config = {
+    test: {
+        name: 'squido',
+        description: 'This is the blog description',
+        twitterHandle: '@mrvautin',
+        baseUrl: 'http://localhost:4965',
+        sourcesExt: 'markdown',
+        sourceDir: 'source',
+        buildDir: 'build',
+        summaryLength: 250,
+        port: 4965,
+        pagination: true,
+        postPerPage: 8,
+        plugins: [
+            {
+                name: 'testplugin',
+                options: {
+                    setting: true,
+                    another: false
+                }
+            },
+            {
+                name: 'dudplugin',
+                options: {
+                    setting: true,
+                    another: false
+                }
+            }
+        ],
+        postBuild: [
+            {
+                name: 'zip',
+                options: {}
+            }
+        ]
+    },
     development: {
         name: 'squido',
         description: 'This is the blog description',
         twitterHandle: '@mrvautin',
         baseUrl: 'http://localhost:4965',
         sourcesExt: 'markdown',
+        templateEngine: 'hbs',
+        templateConfig: {},
+        sourceDir: 'source',
+        buildDir: 'build',
         summaryLength: 250,
         port: 4965,
         pagination: true,
@@ -20,8 +59,10 @@ const config = {
         name: 'squido',
         description: 'This is the blog description',
         twitterHandle: '@mrvautin',
-        baseUrl: 'https://blog.squido.org',
+        baseUrl: 'http://example.com',
         sourcesExt: 'markdown',
+        sourceDir: 'source',
+        buildDir: 'build',
         summaryLength: 250,
         port: 4965,
         pagination: true,
